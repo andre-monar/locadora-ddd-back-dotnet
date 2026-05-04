f3a1275033ce3cc68a15952de78fb86f99508204
andre-mg <andre.m.garcia042@gmail.com>
2026-05-04 02:23:58 -0300

    Add DataCriacao to Carro

---
+++ Infrastructure/Migrations/20260504052315_AddDataCriacaoCarro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataCriacaoCarro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CAR_DATA_CRIACAO",
+                table: "TB_CARRO",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CAR_DATA_CRIACAO",
+                table: "TB_CARRO");
+        }
+    }
+}
